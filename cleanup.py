eb41c1346aca41fd1fd0259e06710af517c99d3d
ankit1w <50029432+ankit1w@users.noreply.github.com>
2020-03-20 18:10:19 +0530

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -13,21 +13,21 @@ def cleanup():
     remove(fr'{gettempdir()}\disable_quick_edit.exe')
     remove(fr'{gettempdir()}\phantomjs.exe')
 
-    for dir_path in iglob(path.join(gettempdir(), "pdfCropMarginsTmpDir_*")):
+    for dir_path in iglob(path.join(gettempdir(), 'pdfCropMarginsTmpDir_*')):
         if path.isdir(dir_path):
             rmtree(dir_path)
 
-    for dir_path in iglob(path.join(gettempdir(), "_MEI*")):
+    for dir_path in iglob(path.join(gettempdir(), '_MEI*')):
         if path.isdir(dir_path) and dir_path != work_dir:
             rmtree(dir_path)
 
-    for dir_path in iglob(path.join(gettempdir(), "tmp*")):
+    for dir_path in iglob(path.join(gettempdir(), 'tmp*')):
         if path.isdir(dir_path + '\\gen_py'):
             rmtree(dir_path)
 
-    for tmp in iglob(path.join(gettempdir(), "tmp*")):
+    for tmp in iglob(path.join(gettempdir(), 'tmp*')):
         if len(tmp.split('\\')[-1]) == 11 and stat(tmp).st_size == 0:
             remove(tmp)
 
-    for tmp_pdf in iglob(path.join(gettempdir(), "ocd_tmp_*")):
+    for tmp_pdf in iglob(path.join(gettempdir(), 'ocd_tmp_*')):
         remove(tmp_pdf)
